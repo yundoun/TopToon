f6a3a2000a8ff5ac87a2969997c87640162061da
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-03-12 16:22:34 +0900

    Feat : MainMenu 생성

--- app/src/main/java/com/example/toptoon/MainActivity.java
+++ app/src/main/java/com/example/toptoon/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.toptoon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuAdapter;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.RvMainMenu);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 4)); // 2열 그리드 설정
+
+        MainMenuAdapter adapter = new MainMenuAdapter();
+        recyclerView.setAdapter(adapter);
+
+        List<MainMenuItem> menuList = new ArrayList<>();
+        menuList.add(new MainMenuItem("연재"));
+        menuList.add(new MainMenuItem("TOP100"));
+        menuList.add(new MainMenuItem("신작"));
+        menuList.add(new MainMenuItem("완결"));
+        menuList.add(new MainMenuItem("추천무료"));
+        menuList.add(new MainMenuItem("전연령"));
+        menuList.add(new MainMenuItem("탑툰쇼츠"));
+        menuList.add(new MainMenuItem("이벤트"));
+
+        adapter.submitList(menuList); // 데이터 설정
+
     }
 }
---
+++ app/src/main/java/com/example/toptoon/MainMenuAdapter.java
@@ -0,0 +1,51 @@
+package com.example.toptoon;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainMenuAdapter extends ListAdapter<MainMenuItem, MainMenuAdapter.MainMenuViewHolder> {
+
+    protected MainMenuAdapter() {
+        super(new DiffUtil.ItemCallback<MainMenuItem>() {
+            @Override
+            public boolean areItemsTheSame(@NonNull MainMenuItem oldItem, @NonNull MainMenuItem newItem) {
+                // 여기서는 단순히 타이틀로 비교
+                return oldItem.getTitle().equals(newItem.getTitle());
+            }
+
+            @Override
+            public boolean areContentsTheSame(@NonNull MainMenuItem oldItem, @NonNull MainMenuItem newItem) {
+                return oldItem.equals(newItem);
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public MainMenuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_rv_row, parent, false);
+        return new MainMenuViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainMenuAdapter.MainMenuViewHolder holder, int position) {
+        MainMenuItem menu = (MainMenuItem) getItem(position);
+        holder.textView.setText(menu.getTitle());
+    }
+
+    static class MainMenuViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public MainMenuViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textViewItem);
+        }
+    }
+}
